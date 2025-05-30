acd878a6366b401234e958a760bd5102d5fd424e
Kritica Agarwal <kriticaagarwal@gmail.com>
2025-05-30 22:35:40 +0530

    remove unused import

--- src/main/java/com/kritica/model/Category.java
+++ src/main/java/com/kritica/model/Category.java
@@ -1,7 +1,6 @@
 package com.kritica.model;
 
 import jakarta.persistence.*;
-import org.hibernate.annotations.ColumnTransformer;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
