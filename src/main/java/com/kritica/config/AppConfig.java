4bddb6ab5a20d6cf2d5795e9d27dd921a6c529f1
Kritica Agarwal <kriticaagarwal@gmail.com>
2025-05-31 19:20:44 +0530

    Add Model mapper to map objets of models and DTO

---
+++ src/main/java/com/kritica/config/AppConfig.java
@@ -0,0 +1,15 @@
+package com.kritica.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+
+@Configurable
+public class AppConfig {
+
+    @Bean
+    public ModelMapper getModelMapper(){
+        return new ModelMapper();
+    }
+
+}
